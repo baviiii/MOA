e63ee97aeb88d8283039faf06146cc91c83c8897
baviiii <aseeshan11@gmail.com>
2025-05-15 14:57:10 +0930

    another dumb misaker

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/wheel-route-pilot/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/MOA/' : '/',
   server: {
     host: "::",
     port: 8080,
